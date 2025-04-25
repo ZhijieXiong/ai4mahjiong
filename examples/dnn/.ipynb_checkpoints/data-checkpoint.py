1765b914404fa0f24f14f4b7b338e6ca4f22404b
ZhijieXiong <18800118477@163.com>
2025-04-26 02:43:36 +0800

    update

--- examples/dnn/.ipynb_checkpoints/data-checkpoint.py
+++
@@ -1,9 +0,0 @@
-from pymj.botzone.SLDataset1 import SLDataset
-
-
-for data_mode in ["train", "valid", "test"]:
-    for mode in ["Play", "Chi", "Peng", "Gang", "AnGang", "BuGang"]:
-        SLDataset.save_data(f"/root/autodl-tmp/mah_jiong_data/{data_mode}",
-                            mode,
-                            f"/root/code/ai4mahjiong/botzone_data/{data_mode}")
-
